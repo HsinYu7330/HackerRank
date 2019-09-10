e446a53828022dd3517e5ce9a20ff1a7e4e2384d
unknown <d0107330@gmail.com>
2019-09-10 10:41:01 +0800

    04. Collections.OrderedDict()

---
+++ 04_collections_OrderedDict.py
@@ -0,0 +1,18 @@
+# Collections.OrderedDict()
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    transaction_dict = OrderedDict()
+    for i in range(n):
+        item = input().split(' ')
+        item_name = ' '.join(item[:-1])
+        item_price = int(item[-1])
+
+        try:
+            transaction_dict[item_name] += item_price
+        except:
+            transaction_dict[item_name] = item_price
+        
+    for name, net_price in transaction_dict.items():
+        print(name, net_price)
