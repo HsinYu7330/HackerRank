f0f40a674cffec04b6de5a08aa0b4e8865dc0538
unknown <d0107330@gmail.com>
2019-09-25 09:55:21 +0800

    17. Set .union() Operation

---
+++ 17_set_union_operation.py
@@ -0,0 +1,10 @@
+# Set .union() Operation
+
+if __name__ == '__main__':
+
+	n = input()
+	a = set(map(int, input().split()))
+	m = input()
+	b = set(map(int, input().split()))
+	
+	print(len(a | b))
