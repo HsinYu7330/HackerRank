be0da9d4de3dcdd620d91468877246a1e9048588
unknown <d0107330@gmail.com>
2019-09-26 09:58:21 +0800

    19. Set .difference() Operation

---
+++ 19_set_difference_operation.py
@@ -0,0 +1,10 @@
+# Set .difference() Operation
+
+if __name__ == '__main__':
+
+	n = input()
+	a = set(map(int, input().split()))
+	m = input()
+	b = set(map(int, input().split()))
+
+	print(len(a-b))
