57e165bfbe80ae89adc3ca8c53674eeab52ae58f
unknown <d0107330@gmail.com>
2020-01-21 16:48:05 +0800

    56. Zeros and Ones

---
+++ 56_zeros_and_ones.py
@@ -0,0 +1,10 @@
+# Zeros and Ones
+
+import numpy
+
+if __name__ == "__main__":
+
+	nums = tuple(map(int, input().split(' ')))
+	
+	print(numpy.zeros(nums, dtype=numpy.int))
+	print(numpy.ones(nums, dtype=numpy.int))
